a3f877e0302482e0db40af9c6e6eaa6f297b3d44
AliceLi11 <1148127906@qq.com>
2020-08-27 20:30:27 +0800

    布局页理解

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    sidebar
+    <el-scrollbar>
+      <el-menu
+        :default-active="activeMenu"
+        :unique-opened="false"
+        :collapse-transition="false"
+        :active-text-color="variables.menuActiveText"
+        :text-color="variables.menuText"
+        :background-color="variables.menuBg"
+        mode="vertical">
+        <sidebar-item v-for="route in routes" :key="route.path"></sidebar-item>
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+import variables from "styles/variables.scss"
+import SidebarItem from "layout/components/Sidebar/SidebarItem"
+export default {
+  components:{
+    SidebarItem
+  },
+  computed:{
+    variables(){
+      return variables
+    },
+    activeMenu(){
+      const route = this.$route;
+      const {meta,path} = route;
+      //if set path,the sidebar will highlight the path you set
+      if(meta.activeMenu){
+        return meta.activeMenu
+      }
+      return path;
+    },
+    routes(){
+      return this.$router.options.routes;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <sidebar/>
+    <div class="main-container">
+      <div>
+        <navbar/>
+      </div>
+      <app-main/>
+    </div>
+  </div>
 </template>
 
 <script>
+import {Navbar,Sidebar,AppMain} from "layout/components"
 export default {
-
+  name:"Layout",
+  components:{
+    Navbar,
+    Sidebar,
+    AppMain
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  @import "styles/variables.scss";
+
 
 </style>
